ca057d7a667e1c20762df31253d48353d6d8f8aa
BrianMohabeer <114097329+BrianMohabeer@users.noreply.github.com>
2023-09-07 16:09:04 -0400

    helloWorld

---
+++ hello.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		//CS176L
+		
+		//Brian Mohabeer
+		
+		//helloWorld
+		System.out.println("Hello World");
+	}
+
+}
