cf1c9bd063dd12e1470168fe350e22c49b20500f
BrianMohabeer <114097329+BrianMohabeer@users.noreply.github.com>
2023-09-08 13:53:57 -0400

    my first java program version 2

--- hello.java
+++ hello.java
@@ -4,12 +4,12 @@ public class hello {
 
 	public static void main(String[] args) 
 	{
-		//CS176L
-		//Brian Mohabeer
-		//helloWorld
-		 
+		/*CS176L
+		  Brian Mohabeer
+		  helloWorld
+	    */
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World this is Brian Mohabeer");
 	}
 
 }
